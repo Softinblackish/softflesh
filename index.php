3217fca53cbf46438311ecb05101573aec7b59b3
Victor Recio <vrecio.intecsol@gmail.com>
2020-08-14 00:21:18 -0400

    index struct

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div>
+            <nav>
+                <ul>
+                <!--Aqui va el logo de la compani-->
+                    <li>inicio</li>
+                    <li>acerca de</li>
+                    <li>sobre nosotros</li>
+                    <li>inventario</li>
+                    <li>estadisticas</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <aside>
+    </aside>
+    <article>
+        <div></div>
+    </article>
+</body>
+</html>
