804904e1415c8ae0ad43cb9e0c3a465ee1154bd4
Victor Recio <vrecio.intecsol@gmail.com>
2020-08-21 00:46:55 -0400

    Cambios en el css

--- views/base.php
+++ views/base.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SoftFlesh</title>
-   <link rel="stylesheet" href="../../css/base.css">
+   <link rel="stylesheet" href="../css/base.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -79,7 +79,9 @@
         
 
 
-        <div id="contenido"></div>
+        <div id="contenido">
+        <?php include_once("articulos/frm_articulos.php"); ?>
+        </div>
     
 
 
