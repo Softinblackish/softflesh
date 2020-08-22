3fad37c6a0e2d2de9e717a07618cca28f6b0d053
Victor Recio <vrecio.intecsol@gmail.com>
2020-08-22 16:24:05 -0400

    cambio de visualizacion de frm_articulos

--- views/articulos/frm_articulos.php
+++ views/articulos/frm_articulos.php
@@ -5,27 +5,42 @@
 <div class="container-articulos">
     <div class="container form-row">
         <form  action="" method="post">
-            <h1>REGISTRO DE ARTICULOS</h1>
+            <div class="cabeza">
+                <h1>REGISTRO DE ARTICULOS</h1>
+            </div>
             <div class="form-row">
-            <input type="text" name="nombre" placeholder ="Nombre del artículo" class="form-control col-md-6">
-            <input type="text" name="codigo" placeholder ="Código" class="form-control col-md-6" >
+                <div class="form-group col-md-6">
+                    <input type="text" name="nombre" placeholder ="Nombre del artículo" class="form-control">
+                </div>
+                <div class="form-group col-md-6">
+                    <input type="text" name="codigo" placeholder ="Código" class="form-control" >
+                </div>
             </div>
-            <div class="form-group">
-            <input type="text" name="descripcion" placeholder ="Descripción" class="form-control" >
-            <input type="text" name="referencia" placeholder ="Referencia" class="form-control col-md-6" >
+
+            <div class="form-row">
+                <input type="text" name="descripcion" placeholder ="Descripción" class="form-control" >
+                <br><br>
+                <input type="text" name="referencia" placeholder ="Referencia" class="form-control col-md-6" >
             </div>
+
             <label for="inputState">Codigo de Impuestos: </label><br>
+            
             <div class="form-row">
-            <input type="text" name="codigo_barra" class="form-control col-md-4" id="inputCity" placeholder="Eje; 0000215EE150" >
-            <select id="inputState" class="form-control col-md-4" name="categoria">
-                    <option selected="">Electricos.</option>
-                    <option>comestibles</option>
-            </select>
-            <select id="inputState" class="form-control col-md-4" name="almacen">
-                    <option selected="">01-Santiago</option>
-                    <option>02-La vega</option>
-            </select>
+                <div class="form-group col-md-4">
+                    <input type="text" name="codigo_barra" class="form-control" id="inputCity" placeholder="Eje; 0000215EE150" >
+                </div>
+                <div class="form-group col-md-4">
+                    <select id="inputState" class="form-control" name="categoria">
+                            <option selected="">Electricos</option>
+                            <option selected="">comestibles</option>
+                    </select>
+                </div>
+                <select id="inputState" class="form-control col-md-4" name="almacen">
+                        <option selected="">01-Santiago</option>
+                        <option>02-La vega</option>
+                </select>
             </div>
+
             <label class="form-check-label" for="gridCheck">
                     Haga click en guardar para registrar este nuevo artículo 
             </label>
