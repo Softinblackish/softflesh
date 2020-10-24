1bd6759ac6ee0a2d3805ab6c32c3613e08ad8d80
Victor Recio <vrecio.intecsol@gmail.com>
2020-10-24 00:51:55 -0400

    cambios para el pdf incompleto

--- views/compras/rpt_compras.php
+++ views/compras/rpt_compras.php
@@ -3,8 +3,12 @@
 require_once('../../scripts/pdf/vendor/autoload.php');
 require_once('plantilla_reporte/plantilla_compras.php');
 
+$desde = empty($_POST['desde']) ? '0000-00-00' : $_POST['desde'];
+$hasta = empty($_POST['hasta']) ? '0000-00-00' : $_POST['hasta'];
+$filtro = $_POST['filtro'];
+
 $mpdf = new \Mpdf\Mpdf();
-$plantilla = getPlantilla();
+$plantilla = getPlantilla($desde,$hasta,$filtro);
 $css = file_get_contents("plantilla_reporte/style.css");
 $mpdf->WriteHTML($css,\Mpdf\HTMLparserMode::HEADER_CSS);
 $mpdf->WriteHTML($plantilla,\Mpdf\HTMLparserMode::HTML_BODY);
