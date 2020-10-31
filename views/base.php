da1f4068205e04ad4216293ade425a8aac8c354e
Victor Recio <vrecio.intecsol@gmail.com>
2020-10-31 12:42:33 -0400

    Cambios de design

--- views/base.php
+++ views/base.php
@@ -17,10 +17,15 @@
     <title>SoftFlesh</title>
    <link rel="stylesheet" href="../../css/base.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    
+   <!--No joder con esta linea evitese problemas....--> 
+   <link rel="stylesheet" href="../../css/Diseño/bootstrap.min.css">
    <!--
    <script src="http://code.jquery.com/jquery-1.9.1.js" type="text/javascript"></script>
    -->
+   
     <script src="../../scripts/js/query/dist/jquery.mask.js" type="text/javascript"></script>
     <script type="text/javascript">
         $(document).ready(function($){
@@ -43,10 +48,13 @@
         });
     </script>
     
+    <!--
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+-->
 <script src="../../scripts/js/base.js" type="text/javascript"> </script>
 </head>
 
