a8d429227da2af3bd3566cf50d89b094d8cb2a07
nafisgonell <nafisgonell07@gmail.com>
2020-10-31 12:02:47 -0400

    Up3

--- scripts/conexion/cone.php
+++ scripts/conexion/cone.php
@@ -1,18 +1,17 @@
 <?php
 
-<<<<<<< HEAD
+
     $conexion = new mysqli("localhost","root","");
+
+    //$conexion = new mysqli("localhost","root","Freddy0770");
+
+
+    //$conexion = new mysqli("localhost","root","");
+
+   // $conexion = new mysqli("localhost","root","1234");
 
    // $conexion = new mysqli("localhost","root","Freddy0770");
-=======
-<<<<<<< HEAD
-    $conexion = new mysqli("localhost","root","");
-=======
-    $conexion = new mysqli("localhost","root","1234");
 
-    $conexion = new mysqli("localhost","root","Freddy0770");
->>>>>>> cc1bd3ea99aefe4b2098cfd6e696e02343950811
->>>>>>> f86b62dd724d93a97d0f553c183b1b7477fe3804
 
 
 
