efd613e820e5be32e844cad6ac64707ac4394379
Victor Recio <vrecio.intecsol@gmail.com>
2020-10-31 11:55:00 -0400

    Resolucion errores en reporte de compras

--- scripts/conexion/cone.php
+++ scripts/conexion/cone.php
@@ -5,7 +5,7 @@
     $conexion = new mysqli("localhost","root","1234");
     //$conexion = new mysqli("localhost","root","Freddy0770");
 
-    $conexion = new mysqli("localhost","root","Freddy07760");
+   // $conexion = new mysqli("localhost","root","Freddy07760");
 
 
 
