928f29da39f01e2a088ff045354b076df3715150
nafisgonell <nafisgonell07@gmail.com>
2020-11-08 00:08:12 -0400

    bt

--- views/Cuentas_por_pagar/frm_cuentas_por_pagar.php
+++ views/Cuentas_por_pagar/frm_cuentas_por_pagar.php
@@ -37,6 +37,9 @@
 				</div>
 			</div>
 			<div class=form-row>			
+				<div class="form-group col-md-12">		
+					<input type="text" name="Articulos"placeholder="Articulos" class="form-control">
+				</div>
 				<div class="form-group col-md-4">		
 					<input type="text" name="cantidad"placeholder="Cantidad" class="form-control">
 				</div>
