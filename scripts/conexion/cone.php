69a72510bb98480662f40403f2d9f4f7bdf99f14
Freddy Pereyra <freddypimpns@gmail.com>
2020-11-29 19:48:49 -0400

    mas cosas de devolucion

--- scripts/conexion/cone.php
+++ scripts/conexion/cone.php
@@ -1,9 +1,9 @@
 <?php
 
 
-    $conexion = new mysqli("localhost","root","1234");
-    //$conexion = new mysqli("localhost","root","");
-    //$conexion = new mysqli("localhost","root","Freddy0770");
+    //$conexion = new mysqli("localhost","root","1234");
+    $conexion = new mysqli("localhost","root","");
+
     
 
 
