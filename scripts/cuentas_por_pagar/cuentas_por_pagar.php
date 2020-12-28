b3c52462842960ec66a9b0ab21615066c0957ecf
nafisgonell <nafisgonell07@gmail.com>
2020-12-28 01:54:44 -0400

    Cuentas por Pagar1

--- scripts/cuentas_por_pagar/cuentas_por_pagar.php
+++ scripts/cuentas_por_pagar/cuentas_por_pagar.php
@@ -15,11 +15,11 @@
     $estatus= $_POST['estatus'];
     $descripcion= $_POST['descripcion'];
     
-    echo $servicios."   ".$inicial."   ".$saldo."   ".$cant_pagos."   ".$tipo_pago."   ".$pago_mensual."   ".$pago_anual."   ".$interes."   ".$moras."   ".$estatus."   ".$descripcion;
+    //echo $servicios."   ".$inicial."   ".$saldo."   ".$cant_pagos."   ".$tipo_pago."   ".$pago_mensual."   ".$pago_anual."   ".$interes."   ".$moras."   ".$estatus."   ".$descripcion;
    
     $Reg_cxp = $conexion->query("INSERT into $empresa.tbl_cuentas_por_pagar (servicios, inicial, saldo, cant_pagos, tipo_pago, pago_mensual, pago_anual, interes, moras, estatus, descripcion)
             values ('$servicios', $inicial, $saldo, $cant_pagos, '$tipo_pago', $pago_mensual, $pago_anual, $interes, $moras, '$estatus', '$descripcion')");
     
 
-    //header('location: ../../views/Cuentas_por_pagar/frm_cuentas_por_pagar.php?registro="si"')
+    header('location: ../../views/Cuentas_por_pagar/frm_cuentas_por_pagar.php?registro="si"')
 ?>
