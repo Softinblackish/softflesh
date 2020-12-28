9c341998c1c1499018813d0d87f577d291fbd7fc
nafisgonell <nafisgonell07@gmail.com>
2020-12-28 01:53:15 -0400

    Cuentas por Pagar

--- views/Cuentas_por_pagar/frm_cuentas_por_pagar.php
+++ views/Cuentas_por_pagar/frm_cuentas_por_pagar.php
@@ -27,25 +27,57 @@
 			</div>
 			<div class=form-row>
 				<div class="form-group col-md-12">
-					<input type="text" name="nombre" placeholder="Nombre del Cliente" class="form-control">
+					<input type="text" name="servicios" placeholder=" Servicios " class="form-control">
 				</div>	
+				<div class="form-group col-md-3">
+					 <label for="inputState">Pago Inicial:</label>
+					<input type="number" name="inicial"  class="form-control" value="0">
+				</div>	
+				<div class="form-group col-md-3">
+				<label for="inputState">Saldo:</label>
+					<input type="nummber" name="saldo"  class="form-control" value="0">
+				</div>	
+				<div class="form-group col-md-3">
+				<label for="inputState">Cant de Pagos:</label>
+					<input type="number" name="cant_pagos" class="form-control">
+				</div>
+				<div class="form-group col-md-3">
+                <label for="inputState">Tipo de Pago:</label>
+                    <select id="inputState" class="form-control" name="tipo_pago">
+                            <option selected="">Corto Plazo</option>
+                            <option selected="">Largo Plazo</option>
+                    </select>
+				</div>
+				<div class="form-group col-md-3">	
+					<label for="inputState">Pago Mensual</label>	
+					<input type="number" name="pago_mensual"  class="form-control">
+				</div>
+				<div class="form-group col-md-3">	
+					<label for="inputState">Pago Anual</label>	
+					<input type="number" name="pago_anual"  class="form-control"value="0">
+				</div>
+				<div class="form-group col-md-3">
+					<label for="inputState">Interes</label>
+					<input type="number" name="interes"  class="form-control" value="0">
+				</div>
+				<div class="form-group col-md-3">
+					<label for="inputState">Moras</label>
+					<input type="number" name="moras"  class="form-control" value="0">
+				</div>
+				<div class="form-group col-md-6">
+                <label for="inputState">Estatus</label>
+                    <select id="inputState" class="form-control" name="estatus">
+                            <option selected="">Pagado</option>
+                            <option selected="">Proceso</option>
+                    </select>
+				</div>
 			</div>
 			<div class=form-row>			
 				<div class="form-group col-md-12">
-					<textarea name="detalle"
+					<textarea name="descripcion"
 					cols="50" rows="3" placeholder="Detalle sobre las condiciones" class="form-control"></textarea>
 				</div>
-			</div>
-			<div class=form-row>			
-				<div class="form-group col-md-12">		
-					<input type="text" name="Articulos"placeholder="Articulos" class="form-control">
-				</div>
-				<div class="form-group col-md-4">
-					<input type="text" name="precio"placeholder="Precio" class="form-control">
-				</div>
-				<div class="form-group col-md-4">	
-					<input type="text" name="total"placeholder="Total" class="form-control">
-				</div>
+					
 			</div>
 			<br>
 			<input type="submit" id="btn" class="btn btn" value="Registrar pago" >
