a9f1006873d525da71fec6bba59a8b3bfb6473dd
nafisgonell <nafisgonell07@gmail.com>
2020-12-27 23:41:43 -0400

    cambios

--- scripts/conexion/cone.php
+++ scripts/conexion/cone.php
@@ -2,7 +2,7 @@
 
 
     ///$conexion = new mysqli("localhost","root","1234");
-    $conexion = new mysqli("localhost","root","");
+       $conexion = new mysqli("localhost","root","");
     //$conexion = new mysqli("localhost","root","Freddy0770");
     
 
