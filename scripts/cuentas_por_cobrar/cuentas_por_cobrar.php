ffb3d96d36415c52fd26c5fffd6bfb8557f5b585
Freddy <Freddypimpns@gmail.com>
2021-02-09 10:10:23 -0400

    cuentas por cobrar scripts

--- scripts/cuentas_por_cobrar/cuentas_por_cobrar.php
+++ scripts/cuentas_por_cobrar/cuentas_por_cobrar.php
@@ -2,16 +2,22 @@
     include("../conexion/cone.php");
     session_start();
     $empresa= $_SESSION["empresa_db"];
-    $nombre= $_POST['nombre']; 
-    $detalle=$_POST["detalle"]; 
-    $cantidad=$_POST['cantidad']; 
-    $price= $_POST['price']; 
-    $total= $_POST['total']; 
+    $cliente= $_POST['cliente']; 
+    $intereses=$_POST["intereses"]; 
+    $meses=$_POST['meses']; 
+    $amortizacion= $_POST['amortizacion']; 
+    $frecuencia= $_POST['frecuencia']; 
+    $cobrador= $_POST['cobrador'];
+    $porciento= $_POST['porciento'];
+    $valor_prestamo= $_POST['valor'];
+    $total_prestamo= $_POST['total'];
+    $cuotas= $_POST['cuotas'];
+
     
    
-        $conexion->query("INSERT into tbl_cxp_cobrar (nombre, detalle, cantidad, price, total)
-            values ('$nombre', '$detalle', '$cantidad', '$price', '$total')");
+        $conexion->query("INSERT into $empresa.tbl_prestamos (id_cliente, meses, cuotas, intereses, amortizacion, frecuencia, cobrador, creado_por, porciento_intereses, valor_prestamo, total_prestamo, estado )
+            values ($cliente, $meses, $cuotas, $intereses, '$amortizacion', '$frecuencia', '$cobrador', 'no seleccion', $porciento, $valor_prestamo, $total_prestamo, 'Pendiente' )");
+        $conexion->query("INSERT into $empresa.tbl_cuentas_c (id_cliente, valor, origen)
+            values ($cliente, $total_prestamo, 'Prestamo' )");
     
-//cxcobrar y cxpagar
-    header('location: ../../views/cuentas_por_cobrar.php?registro="si"')
 ?>
