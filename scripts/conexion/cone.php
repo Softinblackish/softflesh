c9da3df1b68025c09880e585dcc0896906e16996
nafisgonell <nafisgonell07@gmail.com>
2021-02-20 21:01:10 -0400

    erty

--- scripts/conexion/cone.php
+++ scripts/conexion/cone.php
@@ -2,11 +2,11 @@
 
 
     ///$conexion = new mysqli("localhost","root","1234");
-       $conexion = new mysqli("localhost","root","1234");
+      // $conexion = new mysqli("localhost","root","1234");
     //$conexion = new mysqli("localhost","root","Freddy0770");
     
 
 
-    //$conexion = new mysqli("localhost","root","");
+    $conexion = new mysqli("localhost","root","");
     date_default_timezone_set("America/Santo_Domingo");
 ?>
